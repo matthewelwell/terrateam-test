601f3e7163c2ab338afa425b852aa2d1f827d5cd
Matthew Elwell <matthew.elwell@flagsmith.com>
2024-11-18 18:34:53 +0000

    Fix ec2 module again

--- terraform/02_ec2.tf
+++ terraform/02_ec2.tf
@@ -1,20 +1,18 @@
-resource "aws_instance" "coreapi" {
+data "aws_ami" "amazon_linux_2023" {
+  most_recent = true
+  owners = ["amazon"]
+
+  filter {
+    name = "name"
+    values = ["al2023-ami-*-x86_64"]
+  }
+}
+
+resource "aws_instance" "test_instance" {
   ami           = data.aws_ami.amazon_linux_2023.id
   instance_type = "t3.micro"
 
-  key_name = "orange-example-keypair"
-  user_data = data.cloudinit_config.core_api_config.rendered
-
-  vpc_security_group_ids = [
-    aws_security_group.flagsmith_orange_external_sg.id
-  ]
-  availability_zone = data.aws_availability_zones.available.names[0]
-
-  root_block_device {
-    volume_size = 30
-  }
-
   tags = {
-    Name = "core_api"
+    Name = "test-instance"
   }
 }
