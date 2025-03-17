134225ef7d76a9bff3ebd670c33340390e1a6bd6
Matthew Elwell <matthew.elwell@flagsmith.com>
2025-03-17 21:18:05 +0100

    Add feature flags

---
+++ terraform/feature-flags/.terraform.lock.hcl
@@ -0,0 +1,25 @@
+# This file is maintained automatically by "terraform init".
+# Manual edits may be lost in future updates.
+
+provider "registry.terraform.io/flagsmith/flagsmith" {
+  version     = "0.9.1"
+  constraints = "0.9.1"
+  hashes = [
+    "h1:2RcU77WSZHt7BvwlH1xNmffOFGQYVUOxY9cHfpwe4FE=",
+    "zh:0cdf84cf30edff9ba8cfa6a5c4e28a7b6c2bdc12ae22e9bed09a00c8fb1b16db",
+    "zh:11bbda77a1141f46a6e316daad173f56772deb78cd437005831c8b5cb2406eb3",
+    "zh:3cd812370bb6f316829fc7ee3967ce20882787e04152434613611d746536f89a",
+    "zh:3fa3d095403873ce1046c4262ad949132bd208710b320f03046ac328d88cb2e8",
+    "zh:4e851aac160a9025421fab5a796d3963ec59237a13c1d0721e408788ad5a196e",
+    "zh:5fd21956b4228bf9345c7119393c6103da3dafd7e66622d94737305747c34283",
+    "zh:652a852d6a6d691134ed07de73f1bdfc665a048cafa6e8bbc1d01fccf1e8e6fc",
+    "zh:85d28a2ccc4cab5f66e77ef4a28edac8139f5e7bd8983c5919fe911147113be3",
+    "zh:890df766e9b839623b1f0437355032a3c006226a6c200cd911e15ee1a9014e9f",
+    "zh:8c48f33b566ab00f86d08eb9152fa6699170a0b0729c7621bc3b9aa4ac1c1585",
+    "zh:8fbd86534b52008a85f71170e4002db631d467c83db330b1202b240e3c20b69f",
+    "zh:984e43f6dc3e76b47a8f438237c5a413e6633d0c7a003019b593ef6bdcfebb26",
+    "zh:d2456c46bbf406afc19636f7cf87f9fedf2c5732120e3e1e6752007f38171f94",
+    "zh:d7c8e4da746baa3075deb59452d0c6fd4b2210f8fffd99f6c5dd4cbc422d5f1a",
+    "zh:e901e4c3bd19cf603050d85e1c4ca7c6e01d790ed4fe7cc619ec670b43f9fd18",
+  ]
+}
---
+++ terraform/feature-flags/main.tf
@@ -0,0 +1,64 @@
+terraform {
+  required_providers {
+    flagsmith = {
+      source = "Flagsmith/flagsmith"
+      version = "0.9.1"
+    }
+  }
+}
+
+terraform {
+  backend "s3" {
+    bucket = "terraform-state-e5652a1"
+    key    = "feature-flags/terraform.tfstate"
+    region = "eu-west-2"
+  }
+}
+
+# Flagsmith resources
+resource "flagsmith_project" "acme_dot_com" {
+  name                                = "acme.com"
+  organisation_id                     = 16379
+}
+
+resource "flagsmith_environment" "production" {
+  name                                   = "Production"
+  project_id                             = flagsmith_project.acme_dot_com_website.id
+}
+
+resource "flagsmith_feature" "new_homepage_ui" {
+  feature_name    = "new_homepage_ui"
+  project_uuid    = flagsmith_project.acme_dot_com.uuid
+}
+
+resource "flagsmith_segment" "homepage_rollout" {
+  name          = "homepage_rollout"
+  project_uuid  = flagsmith_project.acme_dot_com.uuid
+  feature_id    = flagsmith_feature.new_homepage_ui.id
+
+  rules = [
+    {
+      "rules": [{
+        "conditions": [{
+          "operator": "PERCENTAGE_SPLIT",
+          "value": 10
+        }],
+        "type": "ANY"
+      }],
+      "type": "ALL"
+    }
+  ]
+}
+
+resource "flagsmith_feature_state" "homepage_rollout" {
+  enabled         = true
+  environment_key = flagsmith_environment.production.api_key
+  feature_id      = flagsmith_feature.new_homepage_ui.id
+
+  segment_id        = flagsmith_segment.homepage_rollout.id
+  segment_priority  = 0
+
+  feature_state_value = {
+    string_value = ""
+  }
+}
